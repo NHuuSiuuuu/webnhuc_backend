3962359909e08bf759108da7495f39a3fc7674f9
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2026-01-07 18:30:10 +0700

    Kết nối db mongoose

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const express = require("express")
-const  dotenv = require("dotenv")
+const express = require("express");
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
 
 // Cấu hình env
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT || 3001
+const app = express();
+const port = process.env.PORT || 3001;
 
-app.get("/", (req, res)=> {
-    res.send("hrj hrj r")
-})
+mongoose
+  .connect(
+    "mongodb+srv://nguyenhung2k4gh_db_user:admin@cluster0.sltwzof.mongodb.net/webnhuc"
+  )
+  .then(() => {
+    console.log("Kết nối thành công!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(port, ()=>{
-    console.log("Server running in port", port)
-})
+app.get("/", (req, res) => {
+  res.send("hrj hrj r");
+});
+
+app.listen(port, () => {
+  console.log("Server running in port", port);
+});
---
+++ models/product.model.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const slug = require("mongoose-slug-updater"); // Tự tạo slug từ title
+mongoose.plugin(slug);
+
+const productSchema = new mongoose.Schema({
+  title: String,
+  //   category_id: {
+  //     type: String,
+  //     default: "",
+  //   },
+  description: String,
+  price: Number,
+  discountPercentage: Number,
+  stock: Number,
+  thumbnail: String,
+  featured: String,
+  position: Number,
+  slug: {
+    type: String,
+    slug: "title", // san-pham-1
+    unique: true, // chỉ tạo slug duy nhất
+  },
+
+  //  ai tạo - ngày tạo
+  //   createBy: {
+  //     account_id: String,
+  //     // Thời gian tạo
+  //     createdAt: {
+  //       type: Date,
+  //       default: Date.now(),
+  //     },
+  //   },
+
+  //  ai xóa - ngày xóa
+  //   deletedBy: {
+  //     account_id: String,
+  //     deletedAt: Date,
+  //   },
+  //   updatedBy: [
+  //     {
+  //       account_id: String,
+  //       updatedAt: Date,
+  //     },
+  //   ],
+
+  //   deleted: {
+  //     type: Boolean,
+  //     default: false,
+  //   },
+});
+
+const Product = mongoose.model("Product", productSchema, "Product");
+
+module.exports = Product;
