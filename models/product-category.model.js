7573b73004c9aed96929e2dee81b9194ee047d7f
NHuuSiuuuu <nguyenhung2k4gh@gmail.com>
2026-01-09 17:12:47 +0700

    Model Product Category

---
+++ models/product-category.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const slug = require("mongoose-slug-updater");
+mongoose.plugin(slug);
+
+const productCategorySchema = new mongoose.Schema(
+  {
+    title: String, 
+    description: {
+        type: String,
+        default: ""
+    },
+    thumbnail: String,
+    status: String,
+    position: Number,
+    slug: {
+      type: String,
+      slug: "title", 
+      unique: true   // chỉ tạo slug duy nhất
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const ProductCategory = mongoose.model("ProductCategory", productCategorySchema, "products-category"); // Đối số thứ 3 là bảng colection
+
+module.exports = ProductCategory;
